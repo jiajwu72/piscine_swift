cb00ae8ccce406761fd471c4c703a0992ff405da
jiajun wu <jiajunwu@MacBook-Pro-de-jiajun.local>
2019-10-15 16:06:23 +0200

    1st push

---
+++ JO3/JO3/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  JO3
+//
+//  Created by jiajun wu on 15/10/2019.
+//  Copyright © 2019 jiajun wu. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ JO3/JO3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JO3
+//
+//  Created by jiajun wu on 15/10/2019.
+//  Copyright © 2019 jiajun wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
