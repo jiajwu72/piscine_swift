687067f0e08e08c05e33346ac6e9ebf8a7ee65de
Jiajun WU <jwu@e3r4p1.42.fr>
2019-10-16 15:33:19 +0200

    add J04

---
+++ J04/J04/J04/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  J04
+//
+//  Created by Jiajun WU on 10/16/19.
+//  Copyright © 2019 Jiajun WU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
