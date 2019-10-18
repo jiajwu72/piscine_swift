fe399fb7765ec0c1c3bf6c49143b2b922c6f80be
Jiajun WU <jwu@e3r4p5.42.fr>
2019-10-18 14:58:48 +0200

    add *

---
+++ J004/J004/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  J004
+//
+//  Created by Jiajun WU on 10/17/19.
+//  Copyright © 2019 Jiajun WU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ j05/j05/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  j05
+//
+//  Created by Jiajun WU on 10/18/19.
+//  Copyright © 2019 Jiajun WU. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import  CoreLocation
+
+class FirstViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
