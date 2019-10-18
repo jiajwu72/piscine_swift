5768ec71028170e80b65b94f27e3259a9daf6268
Jiajun WU <jwu@e3r4p5.42.fr>
2019-10-18 15:41:26 +0200

    add map

---
+++ map/map/MapViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MapViewController.swift
+//  map
+//
+//  Created by Jiajun WU on 10/18/19.
+//  Copyright © 2019 Jiajun WU. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController,MKMapViewDelegate{
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ map/map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  map
+//
+//  Created by Jiajun WU on 10/18/19.
+//  Copyright © 2019 Jiajun WU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
