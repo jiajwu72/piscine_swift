24dd8b71e802d72ac96204c4be85f105926af9b6
Jiajun WU <jwu@e3r11p1.42.fr>
2019-10-23 13:08:25 +0200

    add j08

---
+++ J08/jwu2019/Example/Pods/Target Support Files/jwu2019/jwu2019-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_jwu2019 : NSObject
+@end
+@implementation PodsDummy_jwu2019
+@end
---
+++ J08/jwu2019/Example/jwu2019/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  jwu2019
+//
+//  Created by jiajwu72 on 10/22/2019.
+//  Copyright (c) 2019 jiajwu72. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
