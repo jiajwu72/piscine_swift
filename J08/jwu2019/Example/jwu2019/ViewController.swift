81f33916836afb27258fe1843890be1b7b475333
jiajun wu <jiajunwu@MacBook-Pro-de-jiajun.local>
2019-10-23 17:50:52 +0200

    add J08

--- J08/jwu2019/Example/jwu2019/ViewController.swift
+++ J08/jwu2019/Example/jwu2019/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import CoreData
+//import jwu2019
 
 class ViewController: UIViewController {
 
+    //var manager:ArticleManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        /*
+        self.manager=ArticleManager(completion: {
+            DispatchQueue.main.async {
+                //return
+                self.show()
+            }
+        })
+         */
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,5 +32,52 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /*
+    func show(){
+        //print("show")
+        //return
+        guard let article1 = manager?.newArticle() else { print("ERROR : COULD NOT CREATE ARTICLE\n"); return }
+        //return
+        article1.titre = "Bonjour 1"
+        article1.content = "1ère article en français"
+        article1.langue = "fr"
+        article1.image = nil
+        article1.dateCreate = NSDate()
+        article1.dateModif = NSDate()
+        
+        //return 
+        guard let article2 = manager?.newArticle() else { print("ERROR : COULD NOT CREATE ARTICLE\n"); return }
+        article2.titre = "Bonjour 2"
+        article2.content = "2ème article en français"
+        article2.langue = "fr"
+        article2.image = nil
+        article2.dateCreate = NSDate()
+        article2.dateModif = NSDate()
+        
+        
+        guard let article3 = manager?.newArticle() else { print("ERROR : COULD NOT CREATE ARTICLE\n"); return }
+        article3.titre = "Bonjour 3"
+        article3.content = "3ème article en français"
+        article3.langue = "fr"
+        article3.image = nil
+        article3.dateCreate = NSDate()
+        article3.dateModif = NSDate()
+        
+        guard let article4 = manager?.newArticle() else { print("ERROR : COULD NOT CREATE ARTICLE\n"); return }
+        article4.titre = "Hello 4"
+        article4.content = "4th article in english"
+        article4.langue = "fr"
+        article4.image = nil
+        article4.dateCreate = NSDate()
+        article4.dateModif = NSDate()
+        
+        print("Created 4 articles :\n\(article1.description)\n\(article2.description)\n\(article3.description)\n\(article4.description)")
+        
+        self.manager?.save()
+
+        let allArticles = self.manager?.getAllArticles()
+        print("All articles in our library : \n\n\(allArticles!)")
+    }
+    */
 }
 
--- J08/jwu2019/jwu2019.podspec
+++ J08/jwu2019/jwu2019.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
   s.name             = 'jwu2019'
   s.version          = '0.1.0'
-  s.summary          = 'A short description of jwu2019.'
+  s.summary          = 'a day for create a cocoapod.'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -18,7 +18,7 @@ Pod::Spec.new do |s|
 #   * Finally, don't worry about the indent, CocoaPods strips it!
 
   s.description      = <<-DESC
-TODO: Add long description of the pod here.
+piscine d08, a first pod 
                        DESC
 
   s.homepage         = 'https://github.com/jiajwu72/jwu2019'
@@ -37,6 +37,6 @@ TODO: Add long description of the pod here.
   # }
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
-  # s.frameworks = 'UIKit', 'MapKit'
+   s.frameworks = 'CoreData'
   # s.dependency 'AFNetworking', '~> 2.3'
 end
